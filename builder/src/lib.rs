db03b67bd8fbda837f8f004b6626b9416b1e3b7f
Ryume Taguchi <tgcrym01@gmail.com>
2023-03-30 01:04:04 +0900

    Pass test-02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,20 @@
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
-    TokenStream::new()
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = &input.ident;
+    let builder_ident = syn::Ident::new(&format!("{}Builder", ident), ident.span());
+    let expanded = quote!{
+        pub struct #builder_ident {}
+
+        impl #ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {}
+            }
+        }
+    };
+    expanded.into()
 }
